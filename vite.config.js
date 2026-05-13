bfbad39f552adec15bc32ee1b28acd0b85d36f6b
Peter_Butao <peterethanbutao@gmail.com>
2026-05-13 12:07:58 +0200

    remove assetfilename

--- vite.config.js
+++ vite.config.js
@@ -12,19 +12,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         entryFileNames: 'js/[name]-[hash].js',
-        chunkFileNames: 'js/[name]-[hash].js',
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/png|jpe?g|gif|svg|webp|ico/.test(ext)) {
-            return `images/[name]-[hash][extname]`
-          } else if (/woff|woff2|eot|ttf|otf/.test(ext)) {
-            return `fonts/[name]-[hash][extname]`
-          } else if (ext === 'css') {
-            return `css/[name]-[hash][extname]`
-          }
-          return `[name]-[hash][extname]`
-        },
+        chunkFileNames: 'js/[name]-[hash].js'
       },
     },
   },
